1a441560ea46d2ef021e6369ed014e988d769b57
Manny Jim G. Corbo <mjcorbo9394@gmail.com>
2025-10-29 16:25:16 +0800

    dark mode

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,11 +8,11 @@ export default function Navbar() {
                     <span className="text-emerald-600">WORLD</span>
                 </h1>
                 <input type="checkbox" id="menu-toggle" className="peer hidden" />
-                <label for="menu-toggle" className="black lg:text-2xl text-4xl cursor-pointer md:hidden">
+                <label for="menu-toggle" className="dark:text-black lg:text-2xl text-4xl cursor-pointer md:hidden">
                     ☰
                 </label>
                 <ul className="peer-checked:flex hidden flex-col md:flex md:flex-row md:space-x-6 mt-4 md:mt-0 bg-gray-200 md:bg-transparent p-4 md:p-0 rounded md:rounded-none">
-                    <li><a href={'/'} className="hover:text-amber-600 hover:underline hover:scale-100 duration-300 lg:text-md text-xl">Home</a></li>
+                    <li><a href={'/'} className="dark:text-black hover:text-amber-600 hover:underline hover:scale-100 duration-300 lg:text-md text-xl">Home</a></li>
                     <li><a href={'about'} className="hover:text-amber-600 hover:underline hover:scale-100 duration-300 lg:text-md text-xl">About</a></li>
                     <li><a href={'/contact'} className="hover:text-amber-600 hover:underline hover:scale-100 duration-300 lg:text-md text-xl">Contact</a></li>
                 </ul>
