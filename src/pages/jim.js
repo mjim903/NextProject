8b8b9a4d8d8dcb181639030700a3026a9608d8b9
mjim903 <mjcorbo9394@gmail.com>
2026-02-24 19:25:13 +0800

    updated

--- src/pages/jim.js
+++ src/pages/jim.js
@@ -1,12 +1,15 @@
 import Nav from '../components/nav.js'
 import Hero from './hero.js'
+import About from './about.js'
+import Contact from './contact.js'
 
 export default function Jim() {
     return (
         <>
             <Nav />
             <Hero />
-
+            <About />
+            <Contact />
         </>
     )
 }
